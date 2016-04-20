7683098c08d3f27bab071d65984ad85f964c3e1c
Ian Thomas <ithomas@idealcorp.com>
2016-04-19 21:40:31 -0400

    Updated finish component

--- src/components/finish-component.js
+++ src/components/finish-component.js
@@ -12,7 +12,13 @@ class Finish extends React.Component {
     return (
 
       <div>
-        Finish
+        <h2>CONGRATULATIONS YOU'RE A CANDIDATE!</h2>
+        
+        <section className="finished-logos">
+          <img src="src/images/finished_badge_complete.png" />
+        
+          <img src="src/images/tweet.png" />
+        </section>
       </div>
 
     );
