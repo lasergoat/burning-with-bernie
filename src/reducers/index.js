07076f965da1ce4fce79066139d05f52981f55f3
Daniel <polyesterhat@gmail.com>
2016-04-19 23:53:53 -0400

    remove key

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,6 @@
 
 import { modelReducer, formReducer } from 'react-redux-form';
 
-const testAuth = {
-  name: "Daniel Walker", 
-  id: "10105075530997042", 
-  accessToken: "CAAX8WDQboJABACVNnBxj825iYgZB6ZB7MCUQpaY1BLtnrZCnK1KoK8k0MZBu5zZCTYWx1piFpZCZBa7iSAx4PJX7963uVvTxdO2vJMRmjnlPrOZCTGZBQRmJhUtrCiu1ZAm432bg1D3CLmtdactRmnS9P4ZCPivNfZB0LekUaZBW9287SBZCnYyKh9ZC9vCkQZBtP7HCigZAtGwG0a8XZBHmuxjNlZAUsfYjbOiANDA5wQZD"
-}
-
 const defaultOptions = {
   level: 1, 
   weight: 1, 
